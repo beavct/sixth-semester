7e94088c92bcbd91b98b42bf13cadaa3679d84d6
bea <beatrizvianacosta@usp.br>
2024-08-23 16:34:24 -0300

    Add files via upload
    enunciado

---
+++ intro-to-computing-for-exact-sciences-and-technology-mac0115/correcao_minieps/MiniEP3/miniEP3.jl
@@ -0,0 +1,37 @@
+function calculaArea(tipo, base, altura)
+    #=
+        calculaArea(tipo: Int64, base: Float64, altura: Float64) -> Float64
+        Esta função deve calcular a área de um triângulo ou retângulo de acordo com os argumentos.
+    =#
+
+    return 
+end
+
+function calculaAreaTriangulo(base, altura)
+    #=
+        calculaAreaTriangulo(base: Float64, altura: Float64) -> Float64
+        Esta função deve calcular a área de um triângulo.
+    =#    
+
+    return 
+end
+
+function calculaAreaRetangulo(base, altura)
+    #=
+        calculaAreaRetangulo(base: Float64, altura: Float64) -> Float64
+        Esta função deve calcular a área de um retângulo.
+    =#   
+
+    return 
+end
+
+#=
+    Você pode testar a função para diferentes valores de parâmetros
+=# 
+
+tipo = 1
+base = 2
+altura = 3
+area = calculaArea(tipo,base,altura)
+
+println("A área calculada é ", area)
