24f8c0f60102b98f7548e1384bff6e760422a1f0
bea <beatrizvianacosta@usp.br>
2024-08-23 16:33:41 -0300

    Add files via upload
    enunciado

---
+++ intro-to-computing-for-exact-sciences-and-technology-mac0115/correcao_minieps/MiniEPextra1/miniEPextra1.jl
@@ -0,0 +1,12 @@
+function quaseIgual(num1, num2, epsilon)
+    #=
+        quaseIgual(num1: Float64, num2: Float64, epsilon: Float64) -> Bool
+        Esta função deve verificar se a diferença entre os dois números está dentro da margem de erro
+    =#   
+
+    return 
+end
+
+resultado = quaseIgual(sin(pi/4), 0.7071, 0.0001)
+
+println("O resultado foi ", resultado)
