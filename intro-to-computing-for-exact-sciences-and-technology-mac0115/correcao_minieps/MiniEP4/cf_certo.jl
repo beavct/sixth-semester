80ebc02e8fa47d616b23d765ce227fe0f5e18fbe
Bea <beatrizvianacosta@usp.br>
2024-08-30 15:48:10 -0300

    miniep3 correção, miniep4, ep1?

---
+++ intro-to-computing-for-exact-sciences-and-technology-mac0115/correcao_minieps/MiniEP4/cf_certo.jl
@@ -0,0 +1,43 @@
+# código-fonte que funciona corretamente
+
+function Fibo(n)
+    #=
+        Fibo(n: Int64) -> Int64
+        Esta função deve calcular o n-ésimo número da sequência de Fibonacci.
+        Exemplo: Fibo(0) == 0, Fibo(1) == 1, Fibo(5) == 5.
+    =#
+
+    # Início da implementação da função
+    if n == 0
+        return 0
+    elseif n == 1
+        return 1
+    else
+        return Fibo(n - 1) + Fibo(n - 2)
+    end
+end
+
+function verificaFibo()
+    #=
+        verificaFibo() -> Void
+        Esta função deve testar a função Fibo com pelo menos 3 valores diferentes.
+    =#
+
+    # Testes automatizados da função Fibo
+    println("Teste 1: Fibo(0) == 0")
+    println(Fibo(0) == 0 ? "Passou" : "Falhou")
+
+    println("Teste 2: Fibo(1) == 1")
+    println(Fibo(1) == 1 ? "Passou" : "Falhou")
+
+    println("Teste 3: Fibo(5) == 5")
+    println(Fibo(5) == 5 ? "Passou" : "Falhou")
+
+    # Você pode adicionar mais testes para validar a função
+end
+
+#=
+    Você pode testar a função Fibo utilizando a função verificaFibo.
+=#
+
+verificaFibo()
---
+++ intro-to-computing-for-exact-sciences-and-technology-mac0115/correcao_minieps/MiniEP4/cf_enun.jl
@@ -0,0 +1,31 @@
+function Fibo(n)
+    #=
+        Fibo(n: Int64) -> Int64
+        Esta função deve calcular o n-ésimo número da sequência de Fibonacci.
+        Exemplo: Fibo(0) == 0, Fibo(1) == 1, Fibo(5) == 5.
+    =#
+
+    return 
+end
+
+function verificaFibo()
+    #=
+        verificaFibo() -> Void
+        Esta função deve testar a função Fibo com pelo menos 3 valores diferentes.
+    =#
+
+    # Testes automatizados da função Fibo
+    println("Teste 1: Fibo(x) == y")
+
+    println("Teste 2: Fibo(x) == y")
+
+    println("Teste 3: Fibo(x) == y")
+
+    # Você pode adicionar mais testes para validar a função
+end
+
+#=
+    Você pode testar a função Fibo utilizando a função verificaFibo.
+=#
+
+verificaFibo()
