411810d19099b68ab238cef9aeb3bf915c6dd164
beavct <beatrizvianacosta@usp.br>
2024-11-28 14:10:26 -0300

    alterações

--- intro-to-computing-for-exact-sciences-and-technology-mac0115/correcao_minieps/MiniEP8/miniEP8.jl
+++ intro-to-computing-for-exact-sciences-and-technology-mac0115/correcao_minieps/MiniEP8/miniEP8.jl
@@ -2,7 +2,7 @@ function merge(lista1:: Vector{Float64}, lista2:: Vector{Float64}) :: Vector{Flo
     ret =  Vector{Float64}()
 
     for elem in lista1
-        if elem in lista2
+        if elem in lista2 && !(elem in ret)
             push!(ret, elem)
         end
     end
@@ -10,7 +10,30 @@ function merge(lista1:: Vector{Float64}, lista2:: Vector{Float64}) :: Vector{Flo
     return ret
 end
 
+# Nenhum elemento em comum
+#=
+lista1 = [-121.1, 34.5, 56.8]
+lista2 = [0.3, 45.6, 98,7]
+=#
+
+# Todos os elementos em comum
+#=
+lista1 = [2, 3.4, 5.6, 7, 122]
+lista2 = [2, 3.4, 5.6, 7, 122]
+=#
+
+# Exclusão de elementos repetidos
+#=
+lista1 = [1.1, 2.2, 2.2, 4.4]
+lista2 = [2.2, 4.4, 4.4, 5.5, 6.6]
+=#
+
+# Alguns elementos em comum
 lista1 = [1.1, 2.3, 3.5, 4.7]
 lista2 = [2.3, 3.5, 5.6]
+#=
+SAÍDA:
+[2.3, 3.5]
+=#
 
-println(merge(lista1, lista2))
+println("Resultado do merge: $(merge(lista1, lista2))")
